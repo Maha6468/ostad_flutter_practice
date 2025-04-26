9d0a7af72d97d4c7380cd85ec7eeba20d6c563a4
Maha6468 <motherma7633@gmail.com>
2025-04-27 03:02:25 +0600

    scroll Row and Column

--- lib/Flutter_Practice/Module_7/flutter_1.dart
+++ lib/Flutter_Practice/Module_7/flutter_1.dart
@@ -7,73 +7,238 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        titleTextStyle:TextStyle(
-          fontSize: 30,
-          color:Colors.lightGreen,
-          fontWeight:FontWeight.w900,
-          backgroundColor: Colors.red,
-          fontStyle: FontStyle.italic,
-
-        ),
-        leading:Icon(
-          Icons.add_a_photo,
-          size:25.5,
-          color: Colors.white,
-
-        ),
-        toolbarHeight: 60.5,
-        titleSpacing: 50.1,
-
-        actions:<Widget>[
-          IconButton( icon:Icon(Icons.menu) ,onPressed:(){
-            print('this is manubar');
-          },
-          color: Colors.white,
-
+        title:Text('messenger',
+          style: TextStyle(fontSize:30.5,
+            color:Colors.white,
+            fontWeight: FontWeight.w900,
+            //fontStyle: FontStyle.italic,
           ),
-          IconButton( icon:Icon(Icons.access_alarms) ,onPressed:(){
-            print('this is alarm');
-          },)
-
+        ),
+       backgroundColor:Colors.black ,
+       elevation:10 ,
+        actions: [
+          Icon(Icons.tab,color: Colors.white,),
+          SizedBox(width: 10,),
+          Icon(Icons.facebook,color: Colors.white,),
         ],
-        elevation:100.5,
-        backgroundColor:Colors.cyan,
-        centerTitle:true,
-        title: Text('Flutter 1'),
       ),
-      body:Container(
-
-        height:1000 ,
-        width: 1000,
-        color:Colors.lightGreenAccent,
-
-        child:Column(
-          //mainAxisAlignment:MainAxisAlignment.spaceBetween,
-          mainAxisAlignment:MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.end,
+      body: Container(
+        color: Colors.black,
+        child: Column(
           children: [
-            SizedBox(height: 100,),
-            Text('maha'),
-            Text('ma'),
-            Text('ammu'),
-            Text('brother'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children:[
-                Text('this is row'),
+            Container(
+              height: 40,
+              margin:EdgeInsets.symmetric(vertical: 2,horizontal: 20) ,
+              padding: EdgeInsets.all(11),
+              //color:Colors.blueGrey,
+              decoration: BoxDecoration(
+                color: Colors.white54,
+                  borderRadius: BorderRadius.only(
+                      topLeft:Radius.circular(50),
+                      topRight: Radius.circular(50),
+                      bottomLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50))),
+              child: Row(
+                children: [
+                  Icon(Icons.search,
+                    color: Colors.black,),
+                  SizedBox(width: 5,),
+                  Text('Search',selectionColor: Colors.white,
+                  ),
+                ],
+              ),
+            ),
 
-                Text('this is row'),
-                Text('this is row'),
-                SizedBox(height: 200,width: 250,),
-                Icon(Icons.add_alert,color: Colors.red,),
-              ]
-            )
-          ]
+            Container(
+              margin: EdgeInsets.only(left: 5),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(1),
+                  topLeft: Radius.circular(1),
+                  bottomRight: Radius.circular(1),
+                  bottomLeft: Radius.circular(1),
+                ),
+              ),
+              //color: Colors.white60,
+               height: 80,
+               width: 580,
+              child:SingleChildScrollView(
+                scrollDirection:Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      child:Center(child: Text('Maha',)),
+                      height:75,
+                      width: 65,
+                      margin: EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        color: Colors.white54,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    Container(
+                      //color: Colors.blue,
+                      child:Center(child: Text('Saim',)),
+                      height:75,
+                      width: 65,
+                      margin: EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        color: Colors.white54,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    Container(
+                      //color: Colors.blue,
+                      child:Center(child: Text('Mariya',)),
+                      height:75,
+                      width: 65,
+                      margin: EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        color: Colors.white54,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    Container(
+                      //color: Colors.blue,
+                      child:Center(child: Text('Rikta',)),
+                      height:75,
+                      width: 65,
+                      margin: EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        color: Colors.white54,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    Container(
+                      //color: Colors.blue,
+                      child:Center(child: Text('Ibnul',)),
+                      height:75,
+                      width: 65,
+                      margin: EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        color: Colors.white54,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    Container(
+                      //color: Colors.blue,
+                      child:Center(child: Text('Naim',)),
+                      height:75,
+                      width: 65,
+                      margin: EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        color: Colors.white54,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    Container(
+                      //color: Colors.blue,
+                      child:Center(child: Text('Yamin',)),
+                      height:75,
+                      width: 65,
+                      margin: EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        color: Colors.white54,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
 
+            Container(
+              margin: EdgeInsets.only(left:10,top: 30),
+              color: Colors.black,
+               height: 500,
+               width: 500,
+              child: SingleChildScrollView(
 
-        )
-
-      )
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 80,
+                      width: 70,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    Container(
+                      height: 80,
+                      width: 70,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    Container(
+                      height: 80,
+                      width: 70,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    Container(
+                      height: 80,
+                      width: 70,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    Container(
+                      height: 80,
+                      width: 70,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    Container(
+                      height: 80,
+                      width: 70,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    Container(
+                      height: 80,
+                      width: 70,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    Container(
+                      height: 80,
+                      width: 70,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    Container(
+                      height: 80,
+                      width: 70,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
