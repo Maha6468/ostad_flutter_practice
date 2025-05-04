b9c5200a5098ca17cf1d2cef557f2e0fece96933
Maha6468 <motherma7633@gmail.com>
2025-05-04 17:58:44 +0600

    home

---
+++ lib/Flutter_Practice/Module_7/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Flutter Demo",
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.green,
+          ),
+        ),
+        home:MyApp()
+    );
+  }
+}
+
