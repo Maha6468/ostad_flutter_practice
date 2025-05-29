799248db34a81406290afce9cefecc93e8c1bf7d
Maha6468 <motherma7633@gmail.com>
2025-05-29 13:00:11 +0600

    LifeCycle, initState, dispose, setState, didChangeDependencies

---
+++ lib/Flutter_Practice/Module_11/class_1_LifeCycle.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import 'lifeCycle_Test.dart';
+
+
+class LifeCycle extends StatefulWidget {
+  const LifeCycle({super.key});
+
+  @override
+  State<LifeCycle> createState() => _LifeCycleState();
+}
+
+class _LifeCycleState extends State<LifeCycle> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('From initState');
+  }
+
+  @override
+  void dispose() {
+    print('Dispose called');
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('This this app bar'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => LifeCycleTest()));
+          },
+          child: Text('Next page'),
+        ),
+      ),
+    );
+  }
+}
